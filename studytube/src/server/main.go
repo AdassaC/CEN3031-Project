f2b0f2888146812a30ea71102ecd0cda0407cf93
CamSanti16 <75475520+CamSanti16@users.noreply.github.com>
2023-01-27 14:31:33 -0500

    Added Server Folder
    Created main.go file

---
+++ studytube/src/server/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+  "github.com/gorilla/mux"
+  "net/http"
+  "os"
+  "log"
+  "your-awesome-project/src/server/utils"
+)
+
+func main() {
+  r := mux.NewRouter()
+
+  r.HandleFunc("/hello-world", helloWorld)
+
+  http.Handle("/", r)
+
+  srv := &http.Server{
+    Handler: r,
+    Addr:    ":" + os.Getenv("PORT"),
+  }
+
+  log.Fatal(srv.ListenAndServe())
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+  var data = struct {
+    Title string `json:"title"`
+  }{
+    Title: "Golang + Angular Starter Kit",
+  }
+
+  jsonBytes, err := utils.StructToJSON(data); if err != nil {
+    fmt.Print(err)
+  }
+
+  w.Header().Set("Content-Type", "application/json")
+  w.Write(jsonBytes)
+  return
+}
