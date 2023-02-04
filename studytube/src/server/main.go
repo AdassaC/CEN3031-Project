5bf3ecbea630a81b37393e7e2f1946f44fbb9f1e
Ryan Kolb <75460213+formulate716@users.noreply.github.com>
2023-02-03 19:54:55 -0500

    making framework for the database WIP

--- studytube/src/server/main.go
+++ studytube/src/server/main.go
@@ -5,11 +5,25 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"context"
+
 
 	"github.com/adassacoimin/CEN3031-Project/studytube/src/server/utils"
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+//Temporary task struct
+type Task struct {
+	ID string `bson:"_id,omitempty"`
+	Description string `bson:"description"`
+	Status string `bson:"status"`
+}
+
 
 func main() {
 	r := mux.NewRouter()
@@ -46,3 +60,40 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsonBytes)
 	return
 }
+
+func TaskList() error {
+	//connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		return fmt.Errorf("Error connecting to MongoDB: %v", err)
+	}
+	defer client.Disconnect(context.TODO())
+
+	//get a handle to the tasks collection
+	collection := client.Database("taskDB").Collection("tasks")
+
+	//unfinished
+}
+
+func AddTask(task Task) error {
+	//func for adding task to tasklist
+	//connect to mongoDB on localhost port 27017
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		return fmt.Errorf("Error connecting to MongoDB: %v", err)
+	}
+	defer client.Disconnect(context.TODO())
+	
+	//get a handle to the tasks collection
+	collection := client.Database("taskDB").Collection("tasks")
+
+	//add the task to the task list
+	_, err = collection.InsertOne(context.TODO(), task)
+	if err != nil {
+		return fmt.Errorf("Error adding task to task list: %v", err)
+	}
+
+	return nil
+
+}
+
