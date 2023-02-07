da52ee47e75cb56e1307412391396efc0cfd5b1d
AdassaC <80273492+AdassaC@users.noreply.github.com>
2023-02-07 16:16:51 -0500

    Add homepage, w/ routes to home, about, report-bug

--- studytube/src/app/app-routing.module.ts
+++ studytube/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Import components and routes to them
+import { AboutComponent } from './pages/about/about.component';
+import { ReportBugsComponent } from './pages/report-bugs/report-bugs.component';
+import { HomeComponent } from './pages/home/home.component';
+
+//Set routes using components
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'report-bugs',component:ReportBugsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- studytube/src/app/app.module.ts
+++ studytube/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ReportBugsComponent } from './pages/report-bugs/report-bugs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ReportBugsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
