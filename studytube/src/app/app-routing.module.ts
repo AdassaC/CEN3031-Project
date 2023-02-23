6f70d4ceb2d796504b020bba4e080371b8c0236c
Valerie Chery <valerie78@ufl.edu>
2023-02-23 10:59:54 -0500

    Linking login with homepage

--- studytube/src/app/app-routing.module.ts
+++ studytube/src/app/app-routing.module.ts
@@ -3,7 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+// route guard
+import { AuthGuard } from './shared/guard/auth.guard';
+
 const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'register-user', component: SignUpComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'verify-email-address', component: VerifyEmailComponent },
+
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 ];
