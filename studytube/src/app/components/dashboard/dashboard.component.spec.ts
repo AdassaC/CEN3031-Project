cd2a93fc237b968f0d69a75ab12de66f180894bc
AdassaC <80273492+AdassaC@users.noreply.github.com>
2023-03-01 23:01:44 -0500

    Add unit tests

--- studytube/src/app/components/dashboard/dashboard.component.spec.ts
+++ studytube/src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,28 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
-  /*beforeEach(async (() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ DashboardComponent ]
     })
     .compileComponents();
 
     //fixture.detectChanges();
-  }));*/
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DashboardComponent);
-    dashboard = fixture.componentInstance;
+    component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create Dashboard', () => {
+  it('should create Dashboard', async() => {
     const fixture = TestBed.createComponent(DashboardComponent);
-    const dashboard = fixture.debugElement.componentInstance;
+    const dashboard = fixture.componentInstance;
     expect(dashboard).toBeTruthy();
   });
 
