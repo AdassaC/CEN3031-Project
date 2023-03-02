9c905f1767a379407d7d32273ebb5eebcce41172
AdassaC <80273492+AdassaC@users.noreply.github.com>
2023-03-01 20:18:42 -0500

    Update karma.config file for unit tests

--- studytube/src/app/components/dashboard/dashboard.component.spec.ts
+++ studytube/src/app/components/dashboard/dashboard.component.spec.ts
@@ -5,7 +5,7 @@ describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
-  beforeEach(async (() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ DashboardComponent ]
     })
@@ -16,7 +16,7 @@ describe('DashboardComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DashboardComponent);
-    component = fixture.componentInstance;
+    dashboard = fixture.componentInstance;
     fixture.detectChanges();
   });
 
@@ -43,12 +43,12 @@ describe('DashboardComponent', () => {
     const fixture = TestBed.createComponent(DashboardComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('User Profile');
-  }));
+  });
 
-  it('should render "title" in a h1 tag', async(() => {
+  it('should render "title" in a h1 tag', async() => {
     const fixture = TestBed.createComponent(DashboardComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('User Profile');
-  }));
+  });
 });
