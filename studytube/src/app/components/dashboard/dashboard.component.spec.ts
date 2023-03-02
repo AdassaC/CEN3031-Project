4632e9110120f5742d49f9922e182ac58bcd8f31
AdassaC <80273492+AdassaC@users.noreply.github.com>
2023-03-01 20:18:42 -0500

    Update karma.config file for unit tests

--- studytube/src/app/components/dashboard/dashboard.component.spec.ts
+++ studytube/src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,38 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
-  beforeEach(async (() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ DashboardComponent ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(DashboardComponent);
-    component = fixture.componentInstance;
+    dashboard = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should creat Dashboard', () => {
-    const fixture = TestBed.createComponent(DashboardComponent);
-    const dashboard = fixture.debugElement.componentInstance;
+  it('should create Dashboard', async() => {
     expect(dashboard).toBeTruthy();
   });
 
-  it(`should have as title 'User Profile'`, async(() => {
+  it(`should have as title 'User Profile'`, async() => {
     const fixture = TestBed.createComponent(DashboardComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('User Profile');
-  }));
+  });
 
-  it('should render "title" in a h1 tag', async(() => {
+  it('should render "title" in a h1 tag', async() => {
     const fixture = TestBed.createComponent(DashboardComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('User Profile');
-  }));
+  });
 });
