48e96f8f7a4006bac707db7b7d4a059f810b930b
CamSanti16 <75475520+CamSanti16@users.noreply.github.com>
2023-03-27 18:47:20 -0400

    restored some of service

--- studytube/src/app/sign-in.service.ts
+++ studytube/src/app/sign-in.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+class Book { 
+  constructor(
+    public title: string,
+    public author: string, 
+  ) {}
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignInService {
+  api: string = 'https://jsonplaceholder.typicode.com/posts';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getBooks() {  
+    return this.http.get<Book[]>(this.api);
+  }
+  
+  postBook(book : Book) {
+    return this.http.post<Book>(this.api, book)
+  }
+
+
 }
+
+
+
