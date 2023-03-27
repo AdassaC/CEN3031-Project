c3f75580d5a1383e7a2383bd70853c524246114f
CamSanti16 <75475520+CamSanti16@users.noreply.github.com>
2023-03-27 19:08:01 -0400

    signInComponent and Service no syntasx rrors

--- studytube/src/app/sign-in.service.ts
+++ studytube/src/app/sign-in.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-class Book { 
+/*export class Book { // what is the difference between interface and a class?
   constructor(
     public title: string,
     public author: string, 
   ) {}
+} */
+export interface Book {
+  title: string; 
+  author: string; 
 }
 
 @Injectable({
@@ -15,12 +20,15 @@ export class SignInService {
   api: string = 'https://jsonplaceholder.typicode.com/posts';
   constructor(private http: HttpClient) {}
 
-  getBooks() {  
+  getBooks():  Observable<Book[]> {  
     return this.http.get<Book[]>(this.api);
   }
   
-  postBook(book : Book) {
-    return this.http.post<Book>(this.api, book)
+  addBook(title: string, author: string) { //addBook(book : Book) {
+    //return this.http.post<Book>(this.api, book)
+    return this.http.post<Book[]>("/books", {
+      title, author
+    })
   }
 
 
