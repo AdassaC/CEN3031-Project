85b63f85c7f6edf34ec184997cd6c590da828919
CamSanti16 <75475520+CamSanti16@users.noreply.github.com>
2023-03-27 17:58:11 -0400

    changed to main.go

--- studytube/src/server/main.go
+++ studytube/src/server/main.go
@@ -4,45 +4,81 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
 
 	"github.com/adassacoimin/CEN3031-Project/studytube/src/server/utils"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 )
 
 func main() {
-	r := mux.NewRouter()
 
-	r.HandleFunc("/hello-world", helloWorld)
-
-	// Solves Cross Origin Access Issue
-	c := cors.New(cors.Options{
-		AllowedOrigins: []string{"http://localhost:4200"},
-	})
-	handler := c.Handler(r)
-
-	srv := &http.Server{
-		Handler: handler,
-		Addr:    ":" + os.Getenv("PORT"),
+	host := "127.0.0.1:8081"
+	if err := http.ListenAndServe(host, httpHandler()); err != nil {
+		log.Fatalf("Failed to listen on %s: %v", host, err)
 	}
 
-	log.Fatal(srv.ListenAndServe())
 }
 
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	var data = struct {
-		Title string `json:"title"`
-	}{
-		Title: "Golang + Angular Starter Kit",
-	}
+// httpHandler creates the backend HTTP router for queries, types,
+// and serving the Angular frontend.
+func httpHandler() http.Handler {
+	router := mux.NewRouter()
+	// Your REST API requests go here
 
-	jsonBytes, err := utils.StructToJSON(data)
-	if err != nil {
-		fmt.Print(err)
-	}
+	router.HandleFunc("/books/{title}/page/{page}", handleBookGet).Methods("GET")
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(jsonBytes)
-	return
+	// Add your routes here.
+	// WARNING: this route must be the last route defined.
+
+	router.PathPrefix("/").Handler(AngularHandler).Methods("GET")
+
+	/**
+	 * We need some headers to be statically prepended to every response.
+	 */
+	return handlers.LoggingHandler(os.Stdout,
+		handlers.CORS(
+			handlers.AllowCredentials(),
+			handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization",
+				"DNT", "Keep-Alive", "User-Agent", "X-Requested-With", "If-Modified-Since",
+				"Cache-Control", "Content-Range", "Range"}),
+			handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}),
+			handlers.AllowedOrigins([]string{"http://localhost:8080"}),
+			handlers.ExposedHeaders([]string{"DNT", "Keep-Alive", "User-Agent",
+				"X-Requested-With", "If-Modified-Since", "Cache-Control",
+				"Content-Type", "Content-Range", "Range", "Content-Disposition"}),
+			handlers.MaxAge(86400),
+		)(router))
 }
+
+type Book struct {
+	Name string `json:"name"`
+}
+
+func handleBookGet(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	title := vars["title"]
+	page := vars["page"]
+
+	fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+}
+
+func getOrigin() *url.URL {
+	origin, _ := url.Parse("http://localhost:4200")
+	return origin
+}
+
+var origin = getOrigin()
+
+var director = func(req *http.Request) {
+	req.Header.Add("X-Forwarded-Host", req.Host)
+	req.Header.Add("X-Origin-Host", origin.Host)
+	req.URL.Scheme = "http"
+	req.URL.Host = origin.Host
+}
+
+// AngularHandler loads angular assets
+var AngularHandler = &httputil.ReverseProxy{Director: director}
