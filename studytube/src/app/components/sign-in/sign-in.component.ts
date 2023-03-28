40a15cb3d156ad9ca7359c0449478ea54da94ae7
CamSanti16 <75475520+CamSanti16@users.noreply.github.com>
2023-03-27 20:21:03 -0400

    Added forms and information, form still needs updated (click) for params

--- studytube/src/app/components/sign-in/sign-in.component.ts
+++ studytube/src/app/components/sign-in/sign-in.component.ts
@@ -12,18 +12,37 @@ import { Book, SignInService } from 'src/app/sign-in.service';
 export class SignInComponent {
   public books: Book[] = [];
   
+private count = 0;  
+
   constructor(
     public authService: AuthService,
     private bookService: SignInService, 
   ) {
-    //this.books = bookService.getBooks();
-    this.bookService.getBooks().subscribe(res => {
-      this.books = res; 
-    })
+    // I think the get is messing everything up 
+    this.books = bookService.get();
+      //this.bookService.getBooks().subscribe(res => {
+      //  this.books = res; 
+      //})
    }
 
-  addBook(title: string, author: string) {
-    this.bookService.addBook(title, author);
+   // need to make sure this works with paramaters
+   // so the front end can input information and add it to the database 
+  addBookTwo(TitleName: string, AuthorName: string) { 
+    console.log('inside of addBook with parameters')
+    this.books = this.bookService.add({
+      title: TitleName,
+      author: AuthorName
+    })
+  }
+  
+   addBook() {    //(title: string, author: string) {
+    console.log('inside of addBook')
+    //this.bookService.addBook(title, author);
+    this.books = this.bookService.add({
+      title:  `Harry Potter ${this.count}`,
+      author: 'Ana Maria'
+    })
+    this.count++; 
     /*
       .then(
       this.books => {
--- studytube/src/app/sign-in.service.ts
+++ studytube/src/app/sign-in.service.ts
@@ -17,20 +17,31 @@ export interface Book {
   providedIn: 'root'
 })
 export class SignInService {
+  private _books: Book[] = [{
+    title: "Test",
+    author: "Testeee"
+  }]
+
   api: string = 'https://jsonplaceholder.typicode.com/posts';
   constructor(private http: HttpClient) {}
+
+  addBook(book : Book) {
+    return this.http.post<Book>("/books", book)
+  }
 
   getBooks():  Observable<Book[]> {  
     return this.http.get<Book[]>(this.api);
   }
-  
-  addBook(title: string, author: string) { //addBook(book : Book) {
-    //return this.http.post<Book>(this.api, book)
-    return this.http.post<Book[]>("/books", {
-      title, author
-    })
+
+  add(book : Book) { //(title: string, author: string) { //addBook(book : Book) {
+    this._books.push(book);
+    return this._books;
+    //return this.http.post<Book[]>("/books", book)
   }
 
+  get(): Book[]{
+    return this._books;
+  }
 
 }
 
