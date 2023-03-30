4d0a691acb99154c3bd0af14f20fee41feb91fd9
AdassaC <80273492+AdassaC@users.noreply.github.com>
2023-03-29 22:18:39 -0400

    Change route for task styles in new list

--- studytube/src/app/tasksManager/new-list/new-list.component.ts
+++ studytube/src/app/tasksManager/new-list/new-list.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-list',
   templateUrl: './new-list.component.html',
-  styleUrls: ['./new-list.component.css']
+  styleUrls: ['../task-styles.scss','./new-list.component.css']
 })
 export class NewListComponent {
+  /*constructor(private taskService: TaskService) {}
 
+  ngOnInit() {}
+
+  createList(title: string) {
+    this.taskService.createList(title).subscribe((response: any) => {
+      console.log(response);
+    });
+  
+  }*/
 }
