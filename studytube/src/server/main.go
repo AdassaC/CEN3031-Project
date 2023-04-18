54f652134091049b4e95411f24085e913a66983b
CamSanti16 <75475520+CamSanti16@users.noreply.github.com>
2023-04-18 12:30:00 -0400

    changes to the service file

--- studytube/src/server/main.go
+++ studytube/src/server/main.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
 
 	"github.com/adassacoimin/CEN3031-Project/studytube/src/server/utils"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"github.com/rs/cors"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -22,40 +24,76 @@ type Song struct{
      userID string
     }
 type Playlist struct{
-	playlistName string
-	songs []Song
-	userID string
+	PlaylistName string
+	Songs [100]Song
+	UserID string
 }
 
 func main() {
-	r := mux.NewRouter()
 
-
-	// Spotify helper functions in Go 
-	r.HandleFunc("/addsong/{playlistName}/title/{songName}/artist/{artist}/trackURL/{url}", addSongToSpotifyPlaylist)
-	r.HandleFunc("/removetrack/{playlistName}/title/{songName}/artist/{artist}", removeSongFromPlaylist)
-	r.HandleFunc("/updatetrack/{playlistName}/title/{songName}/artist/{artist}/newSong/{newSongName}/newArtist/{newArtistName}/newURL/{updatedURL}", updateTrackOnPlaylist)
-	r.HandleFunc("/createPlaylist/{playlistName}", addPlaylist)
-	r.HandleFunc("/getPlaylist/{playlistName}/{userID}", getPlaylist)
-
-	// Solves Cross Origin Access Issue
-	c := cors.New(cors.Options{
-		AllowedOrigins: []string{"http://localhost:4200"},
-	})
-	handler := c.Handler(r)
-
-	srv := &http.Server{
-		Handler: handler,
-		Addr:    ":" + os.Getenv("PORT"),
+	fmt.Print("inside of main.go")
+	host := "127.0.0.1:4201" // may be 4201
+	//host := "http://localhost:4201"
+	if err := http.ListenAndServe(host, httpHandler()); err != nil {
+		fmt.Print("Failed to listen to " + host)
+		log.Fatalf("Failed to listen on %s: %v", host, err)
+	} else {
+		fmt.Print("Listening to " + host)
 	}
 
-	log.Fatal(srv.ListenAndServe())
+	//DB testing section
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer client.Disconnect(context.TODO())
+
 }
 
-func addPlaylist(w http.ResponseWriter, r *http.Request) error {
+// httpHandler creates the backend HTTP router for queries, types,
+// and serving the Angular frontend.
+func httpHandler() http.Handler {
+
+	fmt.Print("inside of httpHandler in Go")
+	router := mux.NewRouter()
+
+	
+	// Spotify helper functions in Go 
+	router.HandleFunc("/addsong/{playlistName}/title/{songName}/artist/{artist}/trackURL/{url}", addSongToSpotifyPlaylist).Methods("POST")
+	router.HandleFunc("/removetrack/{playlistName}/title/{songName}/artist/{artist}", removeSongFromPlaylist).Methods("POST")
+	router.HandleFunc("/updatetrack/{playlistName}/title/{songName}/artist/{artist}/newSong/{newSongName}/newArtist/{newArtistName}/newURL/{updatedURL}", updateSongFromPlaylist).Methods("POST")
+	router.HandleFunc("/createPlaylist/{playlistName}", addPlaylist).Methods("POST")
+	router.HandleFunc("/getPlaylist/{playlistName}/{userID}", getPlaylist).Methods("GET")
+	
+	
+	// WARNING: this route must be the last route defined.
+
+	router.PathPrefix("/").Handler(AngularHandler).Methods("GET")
+
+	 
+	return handlers.LoggingHandler(os.Stdout,
+		handlers.CORS(
+			handlers.AllowCredentials(),
+			handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization",
+				"DNT", "Keep-Alive", "User-Agent", "X-Requested-With", "If-Modified-Since",
+				"Cache-Control", "Content-Range", "Range"}),
+			handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}),
+			handlers.AllowedOrigins([]string{"http://localhost:4200"}), // maybe should be 4020???
+			handlers.ExposedHeaders([]string{"DNT", "Kxeep-Alive", "User-Agent",
+				"X-Requested-With", "If-Modified-Since", "Cache-Control",
+				"Content-Type", "Content-Range", "Range", "Content-Disposition"}),
+			handlers.MaxAge(86400),
+		)(router))
+}
+
+func addPlaylist(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	playlistName := vars["playlistName"]
-	
+	userID := vars["userID"]
+
+    var songs [100]Song
+
     playlist := Playlist{
         PlaylistName: playlistName,
         Songs:        songs,
@@ -65,7 +103,7 @@ func addPlaylist(w http.ResponseWriter, r *http.Request) error {
     //connect to MongoDB on localhost port 27017
     client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
     if err != nil {
-        return fmt.Errorf("Error connecting to MongoDB: %v", err)
+        fmt.Errorf("Error connecting to MongoDB: %v", err)
     }
     defer client.Disconnect(context.TODO())
 
@@ -74,17 +112,20 @@ func addPlaylist(w http.ResponseWriter, r *http.Request) error {
 
     _, err = collection.InsertOne(context.TODO(), playlist)
     if err != nil {
-        return fmt.Errorf("Error adding task to task list: %v", err)
+        fmt.Errorf("Error adding task to task list: %v", err)
     }
 
-    return nil
 }
 
-func addSongToSpotifyPlaylist(w http.ResponseWriter, r *http.Request) error {
+func addSongToSpotifyPlaylist(w http.ResponseWriter, r *http.Request) {
     vars := mux.Vars(r)
 	playlistName := vars["playlistName"]
 	songName := vars["songName"]
 	artist := vars["artist"]
+
+	// need to get songs from playlist 
+
+
 	//create new Song  struct with string arguments
     // song := Song{
     //  Song:   songName,
@@ -101,7 +142,7 @@ func addSongToSpotifyPlaylist(w http.ResponseWriter, r *http.Request) error {
     //connect to MongoDB on localhost port 27017
     client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
     if err != nil {
-        return fmt.Errorf("Error connecting to MongoDB: %v", err)
+        fmt.Errorf("Error connecting to MongoDB: %v", err)
     }
     defer client.Disconnect(context.TODO())
 
@@ -113,7 +154,7 @@ func addSongToSpotifyPlaylist(w http.ResponseWriter, r *http.Request) error {
     cursor, err := collection.Find(context.Background(), filter)
 
     if err != nil {
-        return fmt.Errorf("Cursor found nothing: %v", err)
+        fmt.Errorf("Cursor found nothing: %v", err)
     }
 
     // fmt.Printf(cursor.Current.String())
@@ -132,9 +173,9 @@ func addSongToSpotifyPlaylist(w http.ResponseWriter, r *http.Request) error {
         fmt.Printf("Not of size 0\n,")
     }
     for i := 0; i < len(theSongs); i++ {
-        if theSongs[i].Artist != "" {
-            theSongs[i+1].Artist = artist
-            theSongs[i+1].Song = songName
+        if theSongs[i].artistName != "" {
+            theSongs[i+1].artistName = artist
+            theSongs[i+1].songName = songName
             break
         }
     }
@@ -146,16 +187,15 @@ func addSongToSpotifyPlaylist(w http.ResponseWriter, r *http.Request) error {
     result, err := collection.UpdateOne(context.Background(), status, update)
 
     if err != nil {
-        return fmt.Errorf("Error updating task status in database: %v", err)
+        fmt.Errorf("Error updating task status in database: %v", err)
     }
     if result.ModifiedCount == 0 {
-        return fmt.Errorf("Task not found in database")
+        fmt.Errorf("Task not found in database")
     }
 
-    return nil
 }
 
-func removeSongFromPlaylist(w http.ResponseWriter, r *http.Request) error {
+func removeSongFromPlaylist(w http.ResponseWriter, r *http.Request) {
     vars := mux.Vars(r)
 	playlistName := vars["playlistName"]
 	songName := vars["songName"]
@@ -163,7 +203,7 @@ func removeSongFromPlaylist(w http.ResponseWriter, r *http.Request) error {
 	//connect to MongoDB on localhost port 27017
     client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
     if err != nil {
-        return fmt.Errorf("Error connecting to MongoDB: %v", err)
+        fmt.Errorf("Error connecting to MongoDB: %v", err)
     }
     defer client.Disconnect(context.TODO())
 
@@ -175,7 +215,7 @@ func removeSongFromPlaylist(w http.ResponseWriter, r *http.Request) error {
     cursor, err := collection.Find(context.Background(), filter)
 
     if err != nil {
-        return fmt.Errorf("Cursor found nothing: %v", err)
+		fmt.Errorf("Cursor found nothing: %v", err)
     }
 
     defer cursor.Close(context.TODO())
@@ -192,9 +232,9 @@ func removeSongFromPlaylist(w http.ResponseWriter, r *http.Request) error {
         fmt.Printf("Not of size 0\n,")
     }
     for i := 0; i < len(theSongs); i++ {
-        if theSongs[i].Artist == artist && theSongs[i].Song == songName {
-            theSongs[i].Artist = ""
-            theSongs[i].Song = ""
+        if theSongs[i].artistName == artist && theSongs[i].songName == songName {
+            theSongs[i].artistName = ""
+            theSongs[i].songName = ""
             break
         }
     }
@@ -206,16 +246,15 @@ func removeSongFromPlaylist(w http.ResponseWriter, r *http.Request) error {
 
     //result, err :=  collection.UpdateOne(context.Background(), songs, songs[]+1)
     if err != nil {
-        return fmt.Errorf("Error updating task status in database: %v", err)
+        fmt.Errorf("Error updating task status in database: %v", err)
     }
     if result.ModifiedCount == 0 {
-        return fmt.Errorf("Task not found in database")
+        fmt.Errorf("Task not found in database")
     }
 
-    return nil
 }
 
-func updateSongFromPlaylist(w http.ResponseWriter, r *http.Request) error {
+func updateSongFromPlaylist(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	playlistName := vars["playlistName"]
 	songName := vars["songName"]
@@ -226,7 +265,7 @@ func updateSongFromPlaylist(w http.ResponseWriter, r *http.Request) error {
     //connect to MongoDB on localhost port 27017
     client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
     if err != nil {
-        return fmt.Errorf("Error connecting to MongoDB: %v", err)
+        fmt.Errorf("Error connecting to MongoDB: %v", err)
     }
     defer client.Disconnect(context.TODO())
 
@@ -238,7 +277,7 @@ func updateSongFromPlaylist(w http.ResponseWriter, r *http.Request) error {
     cursor, err := collection.Find(context.Background(), filter)
 
     if err != nil {
-        return fmt.Errorf("Cursor found nothing: %v", err)
+        fmt.Errorf("Cursor found nothing: %v", err)
     }
 
     defer cursor.Close(context.TODO())
@@ -255,10 +294,10 @@ func updateSongFromPlaylist(w http.ResponseWriter, r *http.Request) error {
         fmt.Printf("Not of size 0\n,")
     }
     for i := 0; i < len(theSongs); i++ {
-        if theSongs[i].Artist == artist && theSongs[i].Song == songName {
-            theSongs[i].Artist = newArtistName
-            theSongs[i].Song = newSongName
-            theSongs[i].url = updatedURL
+        if theSongs[i].artistName == artist && theSongs[i].songName == songName {
+            theSongs[i].artistName = newArtistName
+            theSongs[i].songName = newSongName
+            theSongs[i].trackURL = updatedURL
             break
         }
     }
@@ -270,15 +309,14 @@ func updateSongFromPlaylist(w http.ResponseWriter, r *http.Request) error {
 
     //result, err :=  collection.UpdateOne(context.Background(), songs, songs[]+1)
     if err != nil {
-        return fmt.Errorf("Error updating task status in database: %v", err)
+        fmt.Errorf("Error updating task status in database: %v", err)
     }
     if result.ModifiedCount == 0 {
-        return fmt.Errorf("Task not found in database")
+        fmt.Errorf("Task not found in database")
     }
 
-    return nil
 }
-func deletePlaylist(w http.ResponseWriter, r *http.Request) error {
+func deletePlaylist(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	playlistName := vars["playlistName"]
 	userID := vars["userID"]
@@ -286,7 +324,7 @@ func deletePlaylist(w http.ResponseWriter, r *http.Request) error {
     //connect to MongoDB on localhost port 27017
     client, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017"))
     if err != nil {
-        return fmt.Errorf("Error connecting to MongoDB: %v", err)
+        fmt.Errorf("Error connecting to MongoDB: %v", err)
     }
     defer client.Disconnect(context.Background())
 
@@ -297,16 +335,15 @@ func deletePlaylist(w http.ResponseWriter, r *http.Request) error {
     filter := bson.M{"playlist": playlistName}
     result, err := collection.DeleteOne(context.Background(), filter)
     if err != nil {
-        return fmt.Errorf("Error deleting playlist from database: %v", err)
+        fmt.Errorf("Error deleting playlist from database: %v", err)
     }
     if result.DeletedCount == 0 {
-        return fmt.Errorf("Playlist not found in database")
+		fmt.Errorf("Playlist not found in database")
     }
 
-    return nil
 }
 
-func getPlaylist(w http.ResponseWriter, r *http.Request) error {
+func getPlaylist(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	playlistName := vars["playlistName"]
 	userID := vars["userID"]
@@ -315,7 +352,7 @@ func getPlaylist(w http.ResponseWriter, r *http.Request) error {
     //connect to MongoDB on localhost port 27017
     client, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017"))
     if err != nil {
-        return fmt.Errorf("Error connecting to MongoDB: %v", err)
+        fmt.Errorf("Error connecting to MongoDB: %v", err)
     }
     defer client.Disconnect(context.Background())
 
@@ -327,7 +364,7 @@ func getPlaylist(w http.ResponseWriter, r *http.Request) error {
     cursor, err := collection.Find(context.Background(), filter)
 
     if err != nil {
-        return fmt.Errorf("Cursor found nothing: %v", err)
+        fmt.Errorf("Cursor found nothing: %v", err)
     }
     
     defer cursor.Close(context.TODO())
@@ -344,5 +381,21 @@ func getPlaylist(w http.ResponseWriter, r *http.Request) error {
     //set content type to json
     fmt.Println(jsonBytes)
 
-    return nil
 }
+
+func getOrigin() *url.URL {
+	origin, _ := url.Parse("http://localhost:4200")
+	return origin
+}
+
+var origin = getOrigin()
+
+var director = func(req *http.Request) {
+	req.Header.Add("X-Forwarded-Host", req.Host)
+	req.Header.Add("X-Origin-Host", origin.Host)
+	req.URL.Scheme = "http"
+	req.URL.Host = origin.Host
+}
+
+// AngularHandler loads angular assets
+var AngularHandler = &httputil.ReverseProxy{Director: director}
